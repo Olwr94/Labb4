f9b282e4969905b2e8ae76dcd3a5a69009e8104f
dafamousg <ginokalyun@gmail.com>
2017-09-22 12:00:58 +0200

    Changed name on enum to Walls

--- Lab/Lab/Program.cs
+++ Lab/Lab/Program.cs
@@ -8,9 +8,25 @@ namespace Lab
 {
     class Program
     {
+
+        enum Walls {wall = "#"};
+
         static void Main(string[] args)
         {
-
+            for (int rad = 0; rad < 20; rad++)
+            {
+                for (int kolumn = 0; kolumn < 80; kolumn++)
+                {
+                    if (rad == 19 || rad == 0)//kolumn
+                        Console.Write("#");
+                    else if (kolumn == 79 || kolumn == 0)/*kolumn-9*/
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
         }
     }
     public class Character
@@ -19,7 +35,7 @@ namespace Lab
     }
     public abstract class Map
     {
-        private static string[] map = new string[80 + 1];
+        private static string[,] map = new string[80,40];
     }
     public class Walls : Map
     {
